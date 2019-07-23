0f5ff11a32c8d3091c6fdeb26752d31c61e2ac6a
Sachin Patney <sapatney@microsoft.com>
2019-07-23 13:37:27 -0700

    init

---
+++ index.js
@@ -0,0 +1,26 @@
+const platformLib = require('./lib/platform');
+
+const platform = new platformLib.Platform(0, 1);
+const sleep = platformLib.sleep;
+const Direction = platformLib.Direction;
+
+function runDemo() {
+    await platform.rotate(Direction.FRONT);
+    await sleep(0.1);
+    await platform.dump();
+    await sleep(1);
+
+    await platform.rotate(Direction.RIGHT);
+    await sleep(0.1);
+    await platform.dump();
+    await sleep(1);
+
+    await platform.rotate(Direction.LEFT);
+    await sleep(0.1);
+    await platform.dump();
+    await sleep(1);
+
+    await platform.rotate(Direction.FRONT);
+}
+
+runDemo();
